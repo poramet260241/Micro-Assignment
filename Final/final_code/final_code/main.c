8a741808213ab8b9c526dee492e70569e92f2276
poramet260241 <oblivion2408@gmail.com>
2020-03-13 22:14:10 +0700

    complete code

---
+++ Final/final_code/final_code/main.c
@@ -0,0 +1,39 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+unsigned char counter;
+
+int main(void){
+	DDRB = 0xFF;
+	DDRD = 0x00;
+	PCMSK2 = 0xE0;
+	PCICR = 0x04;
+	counter = 0;
+	sei();
+	while(1){}
+}
+
+ISR(PCINT2_vect){
+	static unsigned char ucInput;
+	_delay_ms(10);
+	ucInput = PIND;
+	ucInput &= 0xE0;
+	ucInput = ucInput >> 5;
+	switch(ucInput){
+		case 1:		counter++;
+					if(counter>15)
+						counter = 0;
+					break;
+		case 2:		counter = 0;
+					break;
+		case 4:		counter--;
+					if(counter < 0)
+					counter = 15;
+					break;
+	}
+					
+	PORTB = counter;
+	PCIFR = 1 << PCIF2;
+}
